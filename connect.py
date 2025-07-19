0fd9b6ba8d4635f6d6bfb76adb37ba0483690107
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-07-20 01:13:41 +0900

    Update connect.py

--- connect.py
+++ connect.py
@@ -36,27 +36,14 @@ if __name__ == '__main__':
     config = config_load()
     
     import psycopg2
-    try:
-        conn = psycopg2.connect('{}://{}:{}@{}:{}/{}'.format(
-            config['internal']['databases'][0]['schema'],
-            config['internal']['databases'][0]['user'],
-            config['internal']['databases'][0]['password'],
-            config['internal']['databases'][0]['host'],
-            config['internal']['databases'][0]['port'],
-            config['internal']['databases'][0]['database'],
-        ))
-
-        cur = conn.cursor()
-    
-        cur.execute('SELECT now(), EXTRACT(epoch FROM CURRENT_TIMESTAMP), trunc(EXTRACT(epoch FROM CURRENT_TIMESTAMP)), to_timestamp(trunc(EXTRACT(epoch FROM CURRENT_TIMESTAMP)));')
-        print(cur.fetchall())
-    except (psycopg2.ProgrammingError):
-        print(traceback.format_exc())
-    except (psycopg2.errors.InsufficientPrivilege):
-        print(traceback.format_exc())
-    
-    conn.commit()
-    
-    cur.close()
-    conn.close()
-
+    with psycopg2.connect('{}://{}:{}@{}:{}/{}'.format(
+        config['internal']['databases'][0]['schema'],
+        config['internal']['databases'][0]['user'],
+        config['internal']['databases'][0]['password'],
+        config['internal']['databases'][0]['host'],
+        config['internal']['databases'][0]['port'],
+        config['internal']['databases'][0]['database'],
+    )) as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT now(), EXTRACT(epoch FROM CURRENT_TIMESTAMP), trunc(EXTRACT(epoch FROM CURRENT_TIMESTAMP)), to_timestamp(trunc(EXTRACT(epoch FROM CURRENT_TIMESTAMP)));')
+            print(cur.fetchall())
